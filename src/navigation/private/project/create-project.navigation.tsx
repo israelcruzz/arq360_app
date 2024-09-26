0daee580fb87d3a6b8ff8ceabefbfc0842dff417
israelcruzz <israelcruz.contato@gmail.com>
2024-09-26 00:36:20 -0300

    feat: add structure to success screen of create project flow

--- src/navigation/private/project/create-project.navigation.tsx
+++ src/navigation/private/project/create-project.navigation.tsx
@@ -1,5 +1,6 @@
 import { createStackNavigator } from "@react-navigation/stack";
 import { Text } from "react-native";
+import { CreateProjectStepSuccessView } from "~/screens/auth/sign-up/success/view";
 import { CreateProjectStepOneView } from "~/screens/private/project/create-project/step-one/view";
 import { CreateProjectStepTreeView } from "~/screens/private/project/create-project/step-tree/view";
 import { CreateProjectStepTwoView } from "~/screens/private/project/create-project/step-two/view";
@@ -8,6 +9,7 @@ export type CreateProjectRootList = {
   createProjectStepOne: undefined;
   createProjectStepTwo: undefined;
   createProjectStepTree: undefined;
+  createProjectStepSuccess: undefined;
 }
 
 const Stack = createStackNavigator<CreateProjectRootList>();
@@ -15,11 +17,12 @@ const Stack = createStackNavigator<CreateProjectRootList>();
 export const CreateProjectNavigation = () => {
   return (
     <Stack.Navigator
-      initialRouteName="createProjectStepTree"
+      initialRouteName="createProjectStepSuccess"
       screenOptions={{ animationEnabled: false, headerShown: false }}>
       <Stack.Screen name="createProjectStepOne" component={CreateProjectStepOneView} />
       <Stack.Screen name="createProjectStepTwo" component={CreateProjectStepTwoView} />
       <Stack.Screen name="createProjectStepTree" component={CreateProjectStepTreeView} />
+      <Stack.Screen name="createProjectStepSuccess" component={CreateProjectStepSuccessView} />
     </Stack.Navigator>
   )
 }
