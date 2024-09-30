d73178c9226485243a10488e3c60d9c4139f002a
israelcruzz <israelcruz.contato@gmail.com>
2024-09-29 23:12:12 -0300

    chore: initial setup to others screens

--- src/navigation/router.tsx
+++ src/navigation/router.tsx
@@ -4,6 +4,7 @@ import OnboardingNavigation from './onboarding-navigator';
 import { useOnboarding } from '~/hooks/onboarding-hooks/useOnboarding';
 import { PrivateTabNavigation } from './private-tab-navigation';
 import { CreateProjectNavigation } from './private/project/create-project.navigation';
+import { ProjectNavigation } from './private/project/project.navigation';
 
 export const Router = () => {
   const { isOnboardingComplete, isLoading } = useOnboarding();
@@ -22,7 +23,7 @@ export const Router = () => {
       <StatusBar barStyle="light-content" backgroundColor="#000" />
       {isOnboardingComplete ? (
         isUserExists ? (
-          <CreateProjectNavigation />
+          <ProjectNavigation />
         ) : (
           <AuthNavigation />
         )
