1a8f3430e1fb4d5a2201fe7a2e2e151267190286
israelcruzz <israelcruz.contato@gmail.com>
2024-09-29 23:54:22 -0300

    feat: add bottom sheet component

--- src/navigation/private/project/project.navigation.tsx
+++ src/navigation/private/project/project.navigation.tsx
@@ -1,5 +1,10 @@
 import { createStackNavigator } from "@react-navigation/stack";
+import { useMemo, useRef } from "react";
 import { Text } from "react-native";
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import BottomSheet, { BottomSheetView } from '@gorhom/bottom-sheet';
+import 'react-native-reanimated';
+
 
 export type ProjectRootList = {
   viewProject: undefined;
@@ -7,8 +12,22 @@ export type ProjectRootList = {
 
 const Stack = createStackNavigator<ProjectRootList>();
 
-const viewProject = () => {
-  return <Text>Olá</Text>
+const ViewProject = () => {
+  const bottomSheetRef = useRef<BottomSheet>(null);
+  const snapPoints = useMemo(() => ['30', '50', '100'], []);
+
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <BottomSheet
+        snapPoints={snapPoints}
+        ref={bottomSheetRef}
+      >
+        <BottomSheetView>
+          <Text>Awesome 🎉</Text>
+        </BottomSheetView>
+      </BottomSheet>
+    </GestureHandlerRootView>
+  )
 }
 
 export const ProjectNavigation = () => {
@@ -16,7 +35,7 @@ export const ProjectNavigation = () => {
     <Stack.Navigator
       initialRouteName="viewProject"
       screenOptions={{ animationEnabled: false, headerShown: false }}>
-      <Stack.Screen name="viewProject" component={viewProject} />
+      <Stack.Screen name="viewProject" component={ViewProject} />
     </Stack.Navigator>
   )
 }
