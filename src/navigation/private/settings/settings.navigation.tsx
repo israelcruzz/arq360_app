da31ed5ccf90f6e2900abc892014dc9e9ee3659d
israelcruzz <israelcruz.contato@gmail.com>
2024-10-05 21:53:39 -0300

    feat(Settings): add structure to change theme screen

--- src/navigation/private/settings/settings.navigation.tsx
+++ src/navigation/private/settings/settings.navigation.tsx
@@ -1,11 +1,12 @@
 import { createStackNavigator } from "@react-navigation/stack";
 import { ChangePasswordViewModel } from "~/screens/private/settings/change-password/view-model";
+import { ChangeThemeViewModel } from "~/screens/private/settings/change-theme/view-model";
 import { EditInformationViewModel } from "~/screens/private/settings/edit-information/view-model";
 
 export type SettingsRootList = {
   editInformation: undefined;
   changePassword: undefined;
-  theme: undefined;
+  changeTheme: undefined;
 }
 
 const Stack = createStackNavigator<SettingsRootList>();
@@ -13,11 +14,12 @@ const Stack = createStackNavigator<SettingsRootList>();
 export const SettingsNavigation = () => {
   return (
     <Stack.Navigator
-      initialRouteName="changePassword"
+      initialRouteName="changeTheme"
       screenOptions={{ animationEnabled: false, headerShown: false }}
     >
       <Stack.Screen name="editInformation" component={EditInformationViewModel} />
       <Stack.Screen name="changePassword" component={ChangePasswordViewModel} />
+      <Stack.Screen name="changeTheme" component={ChangeThemeViewModel} />
     </Stack.Navigator>
   );
 }
