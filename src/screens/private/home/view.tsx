1e83f2e6da38cbb2ba4d3541c8390ea7ab717c3f
israelcruzz <israelcruz.contato@gmail.com>
2024-10-06 19:29:07 -0300

    style(Home): add style file to home screen

--- src/screens/private/home/view.tsx
+++ src/screens/private/home/view.tsx
@@ -4,6 +4,7 @@ import { Badge } from '@/components/badge';
 import { MyCarousel } from '@/components/carousel';
 import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 import { HeadingText } from '@/components/heading-text';
+import { styles } from "@/screens/private/home/styles";
 
 export const Home = () => {
   return (
@@ -34,43 +35,3 @@ export const Home = () => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingVertical: 24,
-    backgroundColor: "#FFFFFF"
-  },
-  header: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    paddingHorizontal: 24,
-  },
-  avatar: {
-    width: 56,
-    height: 56,
-    borderRadius: 9999,
-    objectFit: 'cover',
-  },
-  contentTextArea: {
-    paddingHorizontal: 24,
-    marginTop: 24
-  },
-  headingTextArea: {
-    marginTop: 24,
-    marginBottom: 24,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    color: '#000000',
-  },
-  headingText: {
-    color: '#000000',
-    fontWeight: 'medium',
-    fontSize: 24
-  },
-  seeAllText: {
-    fontSize: 14
-  }
-});
